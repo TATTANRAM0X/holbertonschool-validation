dc30ff1da398f262eba0daecaaaf448c6c60abc9
TATTANRAM0X <jhonatan.ramox@gmail.com>
2023-01-10 22:07:07 -0500

    task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install -y hugo make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
-apt install ./hugo_0.84.0_Linux-64bit.deb
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+apt install ./last.deb
+rm last.deb
 make build
