b689b509ba8f1505ff83362ae8e30eac28408a75
TATTANRAM0X <jhonatan.ramox@gmail.com>
2023-01-13 11:34:31 -0500

    Changes

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install -y hugo make wget
 sudo wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
 sudo apt install ./hugo_0.84.0_Linux-64bit.deb
-make build
+sudo make build
